67e34424edbbf7fe0fce6fd664f08a9448c3b8eb
Silas Beckmann <mail@silasbeckmann.de>
2025-02-12 14:43:28 +0100

    Add json import for potential data serialization and parsing

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from discord.ext import commands, tasks
 from dotenv import load_dotenv
 from datetime import datetime, timezone
 import logging
+import json
 
 # Lade Umgebungsvariablen
 load_dotenv()
